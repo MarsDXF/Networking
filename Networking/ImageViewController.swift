96b1845c5ec804e1c6aa64182823ff111e327c2a
vasiliy <dxfproviewer@gmail.com>
2018-11-21 17:29:38 +0300

    lessens 5. Get Requests.

---
+++ Networking/ImageViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Vasiliy Shaydullin on 21/11/2018.
+//  Copyright © 2018 Vasiliy Shaydullin. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+    
+    func fetchImage() {
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+
+}
+
